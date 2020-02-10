cd288645f42b528d7c6bd1998d991c0828fca519
Julie T <joules2319@gmail.com>
2020-02-09 23:23:01 -0700

    added routes files

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ let PORT = process.env.PORT || 3000;
 //express data handling
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//routes
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//start server
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});

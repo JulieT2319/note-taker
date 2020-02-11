fa431d5d79146dcbd0871e37ebafc7401bf12695
Julie Theriault <joules2319@gmail.com>
2020-02-10 18:32:36 -0700

    added html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,11 @@
-let path = require(path);
+let path = require("path");
+
+module.exports = function (app) {
+	app.get("/notes", function (req, res) {
+		res.sendFile(path.join(__dirname, "../public/notes.html"))
+	})
+	app.get("*", function (req, res) {
+		res.sendFile(path.join(__dirname, "../public/index.html"))
+	})
+
+}

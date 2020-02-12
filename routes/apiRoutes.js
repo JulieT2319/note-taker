3f82a8540f4c5ae9b1eb68935c020f5d888311d0
Julie T <47932789+JulieT2319@users.noreply.github.com>
2020-02-12 11:19:17 -0700

    api route get notes returns json with id of notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,43 @@
 let path = require("path");
 let fs = require("fs");
 
+
+function readFileAsync(path, encoding) {
+	return new Promise(function (resolve, reject) {
+		fs.readFile(path, encoding, function (err, data) {
+			if (err) {
+				return reject(err);
+			}
+
+			resolve(data);
+		});
+	});
+}
 let notes = require("../db/db.json")
 module.exports = function (app) {
+	// notes = fs.readFile("../db/db.json", "utf8", function (err, data) {
+	// 	if (err) {
+	// 		return err;
+	// 	}
+	// 	console.log(data);
+	// 	return data;
+	// });
 	app.get("/api/notes", function (req, res) {
-		// notes = fs.readFile("../db/db.json", (err, data) => {
-		// 	if (err) throw err;
-		// 	return data;
-		// }).then(function () { })
-		// let notesID = notes.forEach(element => {
-		// 	element.id == index(element);
-		// });
+		let idNotes = [];
+		for (i = 0; i < notes.length; i++) {
+			notes[i].id = i + 1;
+		}
+
+
 		res.json(notes);
+		console.log(notes);
+
+		fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err) => {
+			if (err) throw err;
+			console.log('The file has been saved!');
+		});
 	})
+
+
 
 }

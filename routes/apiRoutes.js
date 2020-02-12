9927d9706230eacf17ef9f47ae18897244ca17fc
Julie T <47932789+JulieT2319@users.noreply.github.com>
2020-02-12 13:46:21 -0700

    post api route functional

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,26 +16,23 @@ module.exports = function (app) {
 		// res.json(notes);
 		// console.log(notes);
 	})
+
 	app.post("/api/notes", function (req, res) {
 		let currentNote = req.body;
-		notes.push(currentNote)
-		let notesIDs = [];
-		let maxID = 0;
-		for (i = 0; i < notes.length; i++) {
-			if (notes[i].id) {
-				notesIDs.push(notes[i].id);
-				if (notes[i].id >= maxID) {
-					maxID = notes[i].id
-				}
-			} else {
-				notes[i].id = maxID + 1
+		fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (err, data) {
+			if (err) {
+				return err;
 			}
-		}
-		fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err) => {
-			if (err) throw err;
-			console.log('The file has been saved!');
+			notes = JSON.parse(data);
+			notes.push(currentNote)
+			for (i = 0; i < notes.length; i++) {
+				notes[i].id = i + 1
+			}
+			fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err) => {
+				if (err) throw err;
+				console.log('The note has been added.');
+			});
 		});
-	})
-
-
+	});
 }
+
